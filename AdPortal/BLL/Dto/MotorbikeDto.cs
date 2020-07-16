96b4fedc86f093829ef996513aff2d7e23d54078
Gedas <gedas.ukelis@gmail.com>
2020-07-16 10:29:27 +0300

    Added motorbike search

--- AdPortal/BLL/Dto/MotorbikeDto.cs
+++ AdPortal/BLL/Dto/MotorbikeDto.cs
@@ -10,17 +10,17 @@ namespace BLL.Dto
         public Manufacturer Manufacturer { get; set; }
         public Category Category { get; set; }
         public Condition Condition { get; set; }
-        public DateTime ManufactureDate { get; set; }
+        public DateTime? ManufactureDate { get; set; }
         public VehicleType VehicleType { get; set; }
-        public int EngineCapacity { get; set; }
+        public int? EngineCapacity { get; set; }
         public FuelType FuelType { get; set; }
-        public decimal TireWearOut { get; set; }
-        public int Power { get; set; }
+        public decimal? TireWearOut { get; set; }
+        public int? Power { get; set; }
         public Cooling Cooling { get; set; }
         public Defects Defects { get; set; }
         public bool Registration { get; set; }
         public string Description { get; set; }
-        public decimal Price { get; set; }
+        public decimal? Price { get; set; }
 
     }
 }
--- AdPortal/BLL/MotorbikeService/Queries/Search.cs
+++ AdPortal/BLL/MotorbikeService/Queries/Search.cs
@@ -1,14 +1,11 @@
 ﻿using BLL.Dto;
 using BLL.Infastructure.Exceptions;
 using BLL.Infastructure.UnitOfWork.Interface;
-using BLL.Infastructure.Validation;
 using FluentValidation;
-using FluentValidation.Results;
 using MediatR;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,52 +23,102 @@ namespace BLL.MotorbikeService.Queries
         }
         public class Handler : IRequestHandler<Query, List<MotorbikeDto>>
         {
-
+            public List<object> list = new List<object>();
             private readonly IUnitOfWork uow;
             public Handler(IUnitOfWork uow)
             {
                 this.uow = uow;
             }
 
-            public Task<List<MotorbikeDto>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<List<MotorbikeDto>> Handle(Query request, CancellationToken cancellationToken)
             {
-                MotorbikeDtoValidator validator = new MotorbikeDtoValidator();
-                ValidationResult results = validator.Validate(request.obj);
-
-                if (!results.IsValid)
-                {
-                    validator.ValidateAndThrow(request.obj);
-                }
-             
-                var motorbikes = uow.Mediator.Send(new List.Query());
+                var motorbikes = uow.Mediator.Send(new List.Query()).Result;
 
                 if (motorbikes == null)
                 {
                     throw new StatusCodeException(HttpStatusCode.NotFound, "no Motorbikes found");
                 }
 
-              //ReadPropertiesRecursively(motorbikes.Result[1]);
-
-                //foreach (var PropertyInfo in request.obj.GetType().GetProperties(BindingFlags.Public|BindingFlags.Instance))
-                //{
-                //    var asd = PropertyInfo.Name;
-                //}
-
-                if (request.obj.Manufacturer !=null)
+                if (request.obj.Manufacturer != null)
                 {
-
+                    motorbikes = motorbikes.Where(x => x.Manufacturer.Name == request.obj.Manufacturer.Name).ToList();
+                }
+                if (request.obj.Category != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.Category.Name == request.obj.Category.Name).ToList();
+                }
+                if (request.obj.Condition != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.Condition.VehicCondition == request.obj.Condition.VehicCondition).ToList();
+                }
+                if (request.obj.ManufactureDate != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.ManufactureDate == request.obj.ManufactureDate).ToList();
+                }
+                if (request.obj.VehicleType != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.VehicleType.Type == request.obj.VehicleType.Type).ToList();
+                }
+                if (request.obj.EngineCapacity != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.EngineCapacity == request.obj.EngineCapacity).ToList();
+                }
+                if (request.obj.FuelType != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.FuelType.Type == request.obj.FuelType.Type).ToList();
+                }
+                if (request.obj.TireWearOut != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.TireWearOut == request.obj.TireWearOut).ToList();
+                }
+                if (request.obj.Power != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.Power == request.obj.Power).ToList();
+                }
+                if (request.obj.Cooling != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.Cooling.CoolingType == request.obj.Cooling.CoolingType).ToList();
+                }
+                if (request.obj.Defects != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.Defects.Defect == request.obj.Defects.Defect).ToList();
+                }
+                if (request.obj.Registration != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.Registration == request.obj.Registration).ToList();
+                }
+                if (request.obj.Description != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.Description == request.obj.Description).ToList();
+                }
+                if (request.obj.Price != null)
+                {
+                    motorbikes = motorbikes.Where(x => x.Price == request.obj.Price).ToList();
                 }
                 return motorbikes;
+                //foreach (PropertyInfo property in request.obj.GetType().GetProperties())
+                //{
+                //    if (property.PropertyType.IsClass)
+                //    {
+                //        list.Add(property.PropertyType);
+                //    }
+                //    else
+                //    {
+                //    list.Add(property);
+                //    }
+                //}
+
+
             }
             //private void ReadPropertiesRecursively(Type type)
             //{
-            //    foreach ( PropertyInfo property in type.GetProperties())
+            //    foreach (PropertyInfo property in type.GetProperties())
             //    {
             //        if (property.PropertyType.IsClass)
             //        {
             //            ReadPropertiesRecursively(property.PropertyType);
             //        }
-                    
+            //        list.Add(property);
             //    }
             //}
         }
